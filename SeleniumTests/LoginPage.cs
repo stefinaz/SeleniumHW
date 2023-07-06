da04c4fc33f59edc888270277c2f623c790b57af
Stefania Alexandru <stefaniaalexandru00@gmail.com>
2023-07-06 16:01:41 +0300

    updated tests

---
+++ SeleniumTests/LoginPage.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumDemo
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver driver;
+        private readonly By usernameInputLocator = By.Id("user-name");
+        private readonly By passwordInputLocator = By.Id("password");
+        private readonly By loginButtonLocator = By.Id("login-button");
+        private readonly By errorMessageLocator = By.CssSelector(".error-message-container.error");
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void GoToLoginPage()
+        {
+            driver.Url = "https://www.saucedemo.com/";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        public void EnterUsername(string username)
+        {
+            driver.FindElement(usernameInputLocator).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            driver.FindElement(passwordInputLocator).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(loginButtonLocator).Click();
+        }
+
+        public string GetErrorMessage()
+        {
+            var errorMessage = WaitUntilElementIsVisible(errorMessageLocator);
+            return errorMessage.Text;
+        }
+
+        private IWebElement WaitUntilElementIsVisible(By locator, int timeoutInSeconds = 10)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+        }
+    }
+}
--- SeleniumTests/UnitTest1.cs
+++ SeleniumTests/UnitTest1.cs
@@ -1,110 +1,73 @@
-using NUnit.Framework;
-using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
+using OpenQA.Selenium;
+using SeleniumDemo;
+
 
 namespace SeleniumDemo
 {
+
     public class SauceDemoTests
     {
         private static IWebDriver driver;
-       
+        private LoginPage loginPage;
 
         [SetUp]
         public static void StartBrowser()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Url = "https://www.saucedemo.com/";
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
         }
 
-       
+        [SetUp]
+        public void SetUp()
+        {
+            loginPage = new LoginPage(driver);
+            loginPage.GoToLoginPage();
+        }
 
         [Test]
         public void InvalidUsernameInvalidPassword()
         {
-
-            Login("invalidusername", "invalidpassword");
-            
-            Assert.That(GetErrorMessage(), Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
-
-            
+            loginPage.EnterUsername("invalidusername");
+            loginPage.EnterPassword("invalidpassword");
+            loginPage.ClickLoginButton();
+            Assert.That(loginPage.GetErrorMessage(), Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
         }
-
 
         [Test]
         public void ValidUsernameInvalidPassword()
         {
-            Login("standard_user", "invalidpassword");
-
-            Assert.That(GetErrorMessage(), Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
-
-            
+            loginPage.EnterUsername("standard_user");
+            loginPage.EnterPassword("invalidpassword");
+            loginPage.ClickLoginButton();
+            Assert.That(loginPage.GetErrorMessage(), Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
         }
 
         [Test]
         public void InvalidUsernameValidPassword()
         {
-            Login("invalidusername", "secret_sauce");
-
-            Assert.That(GetErrorMessage(), Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
-
-           
+            loginPage.EnterUsername("invalidusername");
+            loginPage.EnterPassword("secret_sauce");
+            loginPage.ClickLoginButton();
+            Assert.That(loginPage.GetErrorMessage(), Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
         }
 
         [Test]
         public void EmptyUsername()
         {
-            
-            Login("", "secret_sauce");
-
-            Assert.That(GetErrorMessage(), Is.EqualTo("Epic sadface: Username is required"));
-           
-            
-
+            loginPage.EnterUsername("");
+            loginPage.EnterPassword("secret_sauce");
+            loginPage.ClickLoginButton();
+            Assert.That(loginPage.GetErrorMessage(), Is.EqualTo("Epic sadface: Username is required"));
         }
 
         [Test]
         public void EmptyPassword()
         {
-            
-
-            Login("standard_user", "");
-
-            Assert.That(GetErrorMessage(), Is.EqualTo("Epic sadface: Password is required"));
-
-            
-            
-
-        }
-
-
-        private IWebElement WaitUntilElementIsVisible(By locator, int timeoutInSeconds = 10)
-        {
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
-        }
-
-        public void Login(string username, string password)
-        {
-            IWebElement usernameInput = driver.FindElement(By.Id("user-name"));
-            IWebElement passwordInput = driver.FindElement(By.Id("password"));
-            IWebElement loginButton = driver.FindElement(By.Id("login-button"));
-
-            usernameInput.SendKeys(username);
-            passwordInput.SendKeys(password);
-            loginButton.Click();
-
-
-        }
-
-        private string GetErrorMessage()
-        {
-            IWebElement errorMessage = WaitUntilElementIsVisible(By.CssSelector(".error-message-container.error"));
-            return errorMessage.Text;
+            loginPage.EnterUsername("standard_user");
+            loginPage.EnterPassword("");
+            loginPage.ClickLoginButton();
+            Assert.That(loginPage.GetErrorMessage(), Is.EqualTo("Epic sadface: Password is required"));
         }
 
         [TearDown]
